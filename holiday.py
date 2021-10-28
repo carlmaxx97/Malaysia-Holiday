6b88ccf0e083f1e390508173c0c0337343b0011f
carlmaxx97 <47651872+carlmaxx97@users.noreply.github.com>
2021-10-28 16:32:56 +0800

    Add files via upload

--- holiday.py
+++ holiday.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install('tabula-py')
-install('pandas')
-
-# In[5]:
+# In[257]:
 
 
 import tabula as tb
@@ -19,70 +9,87 @@ import pandas as pd
 import re
 
 
-# In[188]:
+# In[269]:
 
 
 file = 'http://www.kabinet.gov.my/bkpp/pdf/hari_kelepasan_am/hka_2021.pdf'
 data  = tb.read_pdf(file,pages = 'all', stream=True)
+print(data)
 
 
-# In[169]:
+# In[270]:
 
 
 df1 = data[0].reset_index().T.reset_index().T[4]
+print(df1)
 
 
-# In[170]:
+# In[271]:
 
 
 df2 = data[1].reset_index().T.reset_index().T[4]
+print(df2)
 
 
-# In[171]:
+# In[272]:
 
 
 df3 = data[3].reset_index().T.reset_index().T[3]
+print(df3)
 
 
-# In[172]:
+# In[273]:
 
 
 l1=list(df1)
 l1.pop(0)
 
 
-# In[173]:
+# In[274]:
 
 
 l2=list(df2)
 l2.pop(0)
 
 
-# In[174]:
+# In[275]:
 
 
 l3=list(df3)
 l3.pop(0)
 
 
-# In[176]:
+# In[277]:
 
 
 holidays=l1+l2+l3
+year="2021"
+holidays = [word.replace("Januari", "January "+year) for word in holidays]
+holidays = [word.replace("Februari", "February "+year) for word in holidays]
+holidays = [word.replace("Mac", "March "+year) for word in holidays]
+holidays = [word.replace("April", "April "+year) for word in holidays]
+holidays = [word.replace("Mei", "May "+year) for word in holidays]
+holidays = [word.replace("Jun", "June "+year) for word in holidays]
+holidays = [word.replace("Julai", "July "+year) for word in holidays]
+holidays = [word.replace("Ogos", "August "+year) for word in holidays]
+holidays = [word.replace("September", "September "+year) for word in holidays]
+holidays = [word.replace("Oktober", "October "+year) for word in holidays]
+holidays = [word.replace("November", "November "+year) for word in holidays]
+holidays = [word.replace("Disember", "December "+year) for word in holidays]
+from datetime import datetime
+
+holidays = [datetime.strptime(word, "%d %B %Y") for word in holidays]
+holidays = [word.strftime("%Y/%m/%d") for word in holidays]
+holidays.sort()
 print(list(dict.fromkeys(holidays)))
 
 
-# In[184]:
+# In[280]:
 
 
 import json
 json_string = json.dumps(list(dict.fromkeys(holidays)))
 print(json_string)
-
-
-# In[187]:
-
-
 with open('holidays.json', 'w') as f:
     json.dump(list(dict.fromkeys(holidays)), f)
 
